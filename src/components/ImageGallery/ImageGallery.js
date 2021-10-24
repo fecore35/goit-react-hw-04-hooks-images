acdd7a87eda5f1f727800bde42df0c52a9e95ed8
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-24 12:34:39 +0300

    Changed ImageGallery component from Class to Hook

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Loader from "react-loader-spinner";
 import pixabayAPI from "../../services/pixabay-api";
@@ -16,51 +16,48 @@ const Status = {
   EMPTY: "empty",
 };
 
-class ImageGallery extends Component {
-  state = {
-    images: [],
-    status: Status.IDLE,
-    error: null,
-    isModalOpen: false,
-    modalImage: null,
-    totalRemainingImages: 0,
-  };
+function ImageGallery({ searchQuery }) {
+  const [status, setStatus] = useState(Status.IDLE);
+  const [images, setImages] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState(null);
+  const [totalRemainingImages, setTotalRemainingImages] = useState(0);
+  const [error, setError] = useState(null);
 
-  async componentDidUpdate(prevProps) {
-    const prevSearchTerms = prevProps.searchQuery;
-    const nextSearchTerms = this.props.searchQuery;
+  useEffect(() => {
+    if (searchQuery === "") {
+      return;
+    }
 
-    if (prevSearchTerms !== nextSearchTerms) {
-      this.setState({ status: Status.PENDING });
+    const getDate = async () => {
+      setStatus(Status.PENDING);
 
       try {
-        API.searchQuery = nextSearchTerms;
+        API.searchQuery = searchQuery;
         API.resetPage();
 
         const images = await API.querySearch();
 
-        this.setState({
-          images,
-          status: images.length > 0 ? Status.RESOLVED : Status.EMPTY,
-          totalRemainingImages: API.totalImages > 12 ? API.totalImages - 12 : 0,
-        });
+        setImages(images);
+        setStatus(() => (images.length > 0 ? Status.RESOLVED : Status.EMPTY));
+        setTotalRemainingImages(() =>
+          API.totalImages > 12 ? API.totalImages - 12 : 0
+        );
       } catch (error) {
-        this.setState({
-          error,
-          status: Status.REJECTED,
-        });
+        setError(error);
+        setStatus(Status.REJECTED);
       }
-    }
-  }
+    };
 
-  onLoadMore = () => {
+    getDate();
+  }, [searchQuery]);
+
+  const onLoadMore = () => {
     API.page = 1;
 
     API.querySearch().then((nextImages) => {
-      this.setState((prevState) => ({
-        images: [...prevState.images, ...nextImages],
-        totalRemainingImages: prevState.totalRemainingImages - 12,
-      }));
+      setImages((state) => [...state, ...nextImages]);
+      setTotalRemainingImages((state) => state - 12);
 
       window.scrollTo({
         top: document.getElementById("gallery-images").scrollHeight,
@@ -69,70 +66,181 @@ class ImageGallery extends Component {
     });
   };
 
-  onOpenModal = (event) => {
+  const onOpenModal = (event) => {
     event.preventDefault();
     const modalImage = event.currentTarget.href;
 
-    this.setState((prevState) => {
-      return { isModalOpen: !prevState.isModalOpen, modalImage };
-    });
+    setIsModalOpen((state) => !state);
+    setModalImage(modalImage);
   };
 
-  render() {
-    const { images, status, error, isModalOpen, modalImage } = this.state;
+  if (status === Status.IDLE) {
+    return <></>;
+  }
 
-    if (status === "idle") {
-      return <></>;
-    }
+  if (status === Status.EMPTY) {
+    return (
+      <p>
+        Sorry, but nothing matched your search terms. Please try again with some
+        different keywords.
+      </p>
+    );
+  }
 
-    if (status === "empty") {
-      return (
-        <p>
-          Sorry, but nothing matched your search terms. Please try again with
-          some different keywords.
-        </p>
-      );
-    }
+  if (status === Status.PENDING) {
+    return (
+      <>
+        <Loader
+          className={s.center}
+          type="ThreeDots"
+          color="#3f51b5"
+          height={100}
+          width={100}
+        />
+      </>
+    );
+  }
 
-    if (status === "pending") {
-      return (
-        <>
-          <Loader
-            className={s.center}
-            type="ThreeDots"
-            color="#3f51b5"
-            height={100}
-            width={100}
-          />
-        </>
-      );
-    }
+  if (status === Status.RESOLVED) {
+    return (
+      <>
+        <ul className={s.gallery} id="gallery-images">
+          <ImageGalleryItem images={images} onOpenModal={onOpenModal} />
+        </ul>
 
-    if (status === "resolved") {
-      return (
-        <>
-          <ul className={s.gallery} id="gallery-images">
-            <ImageGalleryItem images={images} onOpenModal={this.onOpenModal} />
-          </ul>
+        {totalRemainingImages > 0 && <ButtonLoadMore onClick={onLoadMore} />}
 
-          {this.state.totalRemainingImages > 0 && (
-            <ButtonLoadMore onClick={this.onLoadMore} />
-          )}
+        {isModalOpen && (
+          <Modal onOpenModal={onOpenModal}>
+            <img src={modalImage} alt="" />
+          </Modal>
+        )}
+      </>
+    );
+  }
 
-          {isModalOpen && (
-            <Modal onOpenModal={this.onOpenModal}>
-              <img src={modalImage} alt="" />
-            </Modal>
-          )}
-        </>
-      );
-    }
-
-    if (status === "rejected") {
-      return <p>{error.message}</p>;
-    }
+  if (status === Status.REJECTED) {
+    return <p>{error.message}</p>;
   }
 }
+
+// class OldImageGallery extends Component {
+//   state = {
+//     images: [],
+//     status: Status.IDLE,
+//     error: null,
+//     isModalOpen: false,
+//     modalImage: null,
+//     totalRemainingImages: 0,
+//   };
+
+//   async componentDidUpdate(prevProps) {
+//     const prevSearchTerms = prevProps.searchQuery;
+//     const nextSearchTerms = this.props.searchQuery;
+
+//     if (prevSearchTerms !== nextSearchTerms) {
+//       this.setState({ status: Status.PENDING });
+
+//       try {
+//         API.searchQuery = nextSearchTerms;
+//         API.resetPage();
+
+//         const images = await API.querySearch();
+
+//         this.setState({
+//           images,
+//           status: images.length > 0 ? Status.RESOLVED : Status.EMPTY,
+//           totalRemainingImages: API.totalImages > 12 ? API.totalImages - 12 : 0,
+//         });
+//       } catch (error) {
+//         this.setState({
+//           error,
+//           status: Status.REJECTED,
+//         });
+//       }
+//     }
+//   }
+
+//   onLoadMore = () => {
+//     API.page = 1;
+
+//     API.querySearch().then((nextImages) => {
+//       this.setState((prevState) => ({
+//         images: [...prevState.images, ...nextImages],
+//         totalRemainingImages: prevState.totalRemainingImages - 12,
+//       }));
+
+//       window.scrollTo({
+//         top: document.getElementById("gallery-images").scrollHeight,
+//         behavior: "smooth",
+//       });
+//     });
+//   };
+
+//   onOpenModal = (event) => {
+//     event.preventDefault();
+//     const modalImage = event.currentTarget.href;
+
+//     this.setState((prevState) => {
+//       return { isModalOpen: !prevState.isModalOpen, modalImage };
+//     });
+//   };
+
+//   render() {
+//     const { images, status, error, isModalOpen, modalImage } = this.state;
+
+//     if (status === "idle") {
+//       return <></>;
+//     }
+
+//     if (status === "empty") {
+//       return (
+//         <p>
+//           Sorry, but nothing matched your search terms. Please try again with
+//           some different keywords.
+//         </p>
+//       );
+//     }
+
+//     if (status === "pending") {
+//       return (
+//         <>
+//           <Loader
+//             className={s.center}
+//             type="ThreeDots"
+//             color="#3f51b5"
+//             height={100}
+//             width={100}
+//           />
+//         </>
+//       );
+//     }
+
+//     if (status === "resolved") {
+//       return (
+//         <>
+//           <ul className={s.gallery} id="gallery-images">
+//             <ImageGalleryItem images={images} onOpenModal={this.onOpenModal} />
+//           </ul>
+
+//           {this.state.totalRemainingImages > 0 && (
+//             <ButtonLoadMore onClick={this.onLoadMore} />
+//           )}
+
+//           {isModalOpen && (
+//             <Modal onOpenModal={this.onOpenModal}>
+//               <img src={modalImage} alt="" />
+//             </Modal>
+//           )}
+//         </>
+//       );
+//     }
+
+//     if (status === "rejected") {
+//       return <p>{error.message}</p>;
+//     }
+//   }
+// }
 
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string,
