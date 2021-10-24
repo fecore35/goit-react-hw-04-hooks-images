e2c9df6b1873d77b7d76997b5fa8c55f6464aa86
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-24 13:02:58 +0300

    Canged Searchbar component from Class to Hook

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,53 +1,92 @@
-import { Component } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import s from "./Searchbar.module.css";
 
-class Searchbar extends Component {
-  state = {
-    searchQuery: "",
+function Searchbar({ onSubmit }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handlerSearchQuery = (event) => {
+    setSearchQuery(event.target.value);
   };
 
-  handlerSearchQuery = (event) => {
-    this.setState({
-      searchQuery: event.currentTarget.value,
-    });
-  };
-
-  handlerSubmit = (event) => {
+  const handlerSubmit = (event) => {
     event.preventDefault();
 
-    if (this.state.searchQuery.trim() === "") {
+    if (searchQuery.trim() === "") {
       return alert("Enter query search");
     }
 
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: "" });
+    onSubmit(searchQuery);
+    setSearchQuery("");
   };
 
-  render() {
-    const { searchQuery } = this.state;
+  return (
+    <header className={s.header}>
+      <form className={s.form} onSubmit={handlerSubmit}>
+        <button type="submit" className={s.button}>
+          <span className={s.label}>Search</span>
+        </button>
 
-    return (
-      <header className={s.header}>
-        <form className={s.form} onSubmit={this.handlerSubmit}>
-          <button type="submit" className={s.button}>
-            <span className={s.label}>Search</span>
-          </button>
-
-          <input
-            className={s.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={searchQuery}
-            onChange={this.handlerSearchQuery}
-          />
-        </form>
-      </header>
-    );
-  }
+        <input
+          className={s.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchQuery}
+          onChange={handlerSearchQuery}
+        />
+      </form>
+    </header>
+  );
 }
+
+// class OldSearchbar extends Component {
+//   state = {
+//     searchQuery: "",
+//   };
+
+//   handlerSearchQuery = (event) => {
+//     this.setState({
+//       searchQuery: event.currentTarget.value,
+//     });
+//   };
+
+//   handlerSubmit = (event) => {
+//     event.preventDefault();
+
+//     if (this.state.searchQuery.trim() === "") {
+//       return alert("Enter query search");
+//     }
+
+//     this.props.onSubmit(this.state.searchQuery);
+//     this.setState({ searchQuery: "" });
+//   };
+
+//   render() {
+//     const { searchQuery } = this.state;
+
+//     return (
+//       <header className={s.header}>
+//         <form className={s.form} onSubmit={this.handlerSubmit}>
+//           <button type="submit" className={s.button}>
+//             <span className={s.label}>Search</span>
+//           </button>
+
+//           <input
+//             className={s.input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             value={searchQuery}
+//             onChange={this.handlerSearchQuery}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
