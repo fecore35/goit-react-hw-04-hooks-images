9827180844adf61739149a39e164bf34fb49cf51
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-24 12:40:45 +0300

    Changed ImageGalleryItem from Class to Function

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,24 +1,21 @@
-import { Component } from "react";
 import PropTypes from "prop-types";
 import s from "./ImageGalleryItem.module.css";
 
-class ImageGalleryItem extends Component {
-  render() {
-    return this.props.images.map(({ id, webformatURL, largeImageURL }) => {
-      return (
-        <li key={id} id={id} className={s.item}>
-          <a href={largeImageURL} onClick={this.props.onOpenModal}>
-            <img
-              src={webformatURL}
-              data-modal={largeImageURL}
-              alt=""
-              className={s.image}
-            />
-          </a>
-        </li>
-      );
-    });
-  }
+function ImageGalleryItem({ images, onOpenModal }) {
+  return images.map(({ id, webformatURL, largeImageURL }) => {
+    return (
+      <li key={id} id={id} className={s.item}>
+        <a href={largeImageURL} onClick={onOpenModal}>
+          <img
+            src={webformatURL}
+            data-modal={largeImageURL}
+            alt=""
+            className={s.image}
+          />
+        </a>
+      </li>
+    );
+  });
 }
 
 ImageGalleryItem.propTypes = {
