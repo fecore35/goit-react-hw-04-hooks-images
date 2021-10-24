36993d5114e28f76069db77184994555a5c04e62
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-24 12:53:22 +0300

    Changed Modal component from Cllas to Hook

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,32 +1,55 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 import s from "./Modal.module.css";
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.onCloseModal);
-  }
+function Modal({ children, onOpenModal }) {
+  useEffect(() => {
+    window.addEventListener("keydown", onCloseModal);
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.onCloseModal);
-  }
+    return () => {
+      window.removeEventListener("keydown", onCloseModal);
+    };
+  });
 
-  onCloseModal = (e) => {
+  const onCloseModal = (e) => {
     if (e.code === "Escape" || e.currentTarget === e.target) {
-      this.props.onOpenModal(e);
+      onOpenModal(e);
     }
   };
 
-  render() {
-    return createPortal(
-      <div className={s.overlay} onClick={this.onCloseModal}>
-        <div className={s.modal}>{this.props.children}</div>
-      </div>,
-      document.getElementById("modal-root")
-    );
-  }
+  return createPortal(
+    <div className={s.overlay} onClick={onCloseModal}>
+      <div className={s.modal}>{children}</div>
+    </div>,
+    document.getElementById("modal-root")
+  );
 }
+
+// class OldModal extends Component {
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.onCloseModal);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener("keydown", this.onCloseModal);
+//   }
+
+//   onCloseModal = (e) => {
+//     if (e.code === "Escape" || e.currentTarget === e.target) {
+//       this.props.onOpenModal(e);
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={s.overlay} onClick={this.onCloseModal}>
+//         <div className={s.modal}>{this.props.children}</div>
+//       </div>,
+//       document.getElementById("modal-root")
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   onOpenModal: PropTypes.func.isRequired,
